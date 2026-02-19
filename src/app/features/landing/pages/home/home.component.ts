fc79a11c8c8e8576c63395aef9dd946a94782509
Abiel252005 <abielon25@gmail.com>
2026-02-18 21:58:35 -0600

    chore: cleanup unused imports in HomeComponent after merge

--- src/app/features/landing/pages/home/home.component.ts
+++ src/app/features/landing/pages/home/home.component.ts
@@ -4,12 +4,9 @@ import { HeroComponent } from '../../components/hero/hero.component';
 import { AboutComponent } from '../../components/about/about.component';
 import { FeaturesComponent } from '../../components/features/features.component';
 import { ProcessComponent } from '../../components/process/process.component';
-import { StatsComponent } from '../../components/stats/stats.component';
 import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
 import { FaqComponent } from '../../components/faq/faq.component';
 import { ContactComponent } from '../../components/contact/contact.component';
-import { HeaderComponent } from '../../../../core/components/header/header.component';
-import { FooterComponent } from '../../../../core/components/footer/footer.component';
 
 @Component({
     selector: 'app-home',
@@ -20,12 +17,9 @@ import { FooterComponent } from '../../../../core/components/footer/footer.compo
         AboutComponent,
         FeaturesComponent,
         ProcessComponent,
-        StatsComponent,
         TestimonialsComponent,
         FaqComponent,
-        ContactComponent,
-        HeaderComponent,
-        FooterComponent
+        ContactComponent
     ],
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
@@ -33,3 +27,4 @@ import { FooterComponent } from '../../../../core/components/footer/footer.compo
 export class HomeComponent {
 
 }
+
